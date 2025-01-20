4e1bff6ba9e1aa1a5ef071bbb46a30acbe235d4c
Abhoy Sarkar <sarkar.ab07@gmail.com>
2025-01-20 21:05:16 +0530

    backend v1 implemented

--- apps/http-backend/src/routes/room.ts
+++ apps/http-backend/src/routes/room.ts
@@ -1,4 +1,4 @@
-import { encode } from "@repo/backend-common/encoder-decoder";
+import { decode, encode } from "@repo/backend-common/encoder-decoder";
 import { CreateRoomSchema } from "@repo/common/types";
 import { prisma } from "@repo/db/client";
 import { Response, Router } from "express";
@@ -9,6 +9,12 @@ const router: Router = Router();
 interface RoomProps extends AuthReqProps {
   body: {
     slug: string;
+  };
+}
+
+interface ChatProps extends AuthReqProps {
+  params: {
+    roomId: string;
   };
 }
 
@@ -30,6 +36,18 @@ router.post("/room", async (req: RoomProps, res: Response) => {
 
     const slug = validation.data.name;
 
+    const existingRoom = await prisma.room.findFirst({
+      where: {
+        slug: slug,
+      },
+    });
+
+    if (existingRoom) {
+      const roomId = encode({ id: existingRoom.id, slug: existingRoom.slug });
+      res.status(200).json({ message: "Room created", response: roomId });
+      return;
+    }
+
     const response = await prisma.room.create({
       data: {
         slug,
@@ -46,6 +64,39 @@ router.post("/room", async (req: RoomProps, res: Response) => {
     res.status(200).json({ message: "Room created", response: roomId });
   } catch (error) {
     res.status(500).json({ message: "Error creating room" });
+  }
+});
+
+router.get("/chats/:roomId", async (req: ChatProps, res: Response) => {
+  try {
+    const userId = req.userId;
+
+    if (!userId) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
+
+    const roomId = decode(req.params.roomId ?? "");
+
+    if (!roomId) {
+      res.status(400).json({ message: "Invalid roomId" });
+      return;
+    }
+
+    const chats = await prisma.chat.findMany({
+      where: {
+        roomId: roomId.id,
+        userId: userId,
+      },
+      orderBy: {
+        id: "desc",
+      },
+      take: 100,
+    });
+
+    res.status(200).json({ message: "Chat retrieved", chats });
+  } catch (error) {
+    res.status(500).json({ message: "Error getting chat" });
   }
 });
 
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,18 @@
 import { JWT_SECRET } from "@repo/backend-common/config";
+import { decode } from "@repo/backend-common/encoder-decoder";
+import { prisma } from "@repo/db/client";
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
+
+interface User {
+  ws: WebSocket;
+  userId: string;
+  rooms: number[];
+}
 
 const wss = new WebSocketServer({ port: 8080 });
+
+const users: User[] = [];
 
 function checkUser(token: string) {
   try {
@@ -29,7 +39,104 @@ wss.on("connection", function connection(ws, request) {
     return;
   }
 
-  ws.on("message", function message(data) {
-    ws.send("pong");
+  users.push({
+    ws: ws,
+    userId: userId,
+    rooms: [],
+  });
+
+  ws.on("message", async function message(data: string) {
+    const parsedData = JSON.parse(data);
+    if (parsedData.type === "join") {
+      try {
+        const roomId = decode(parsedData.roomId);
+        console.log(roomId);
+        const user = users.find((u) => u.ws === ws);
+
+        if (!user) {
+          return;
+        }
+
+        const existingRoom = await prisma.room.findFirst({
+          where: {
+            id: roomId.id,
+            slug: roomId.slug,
+          },
+        });
+
+        if (!existingRoom) {
+          console.log(existingRoom);
+          console.log("no such rooms exist");
+          return;
+        }
+        user.rooms.push(roomId.id);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    if (parsedData.type === "leave") {
+      try {
+        const user = users.find((u) => u.ws === ws && u.userId === userId);
+        if (!user) {
+          return;
+        }
+        const roomId = decode(parsedData.roomId);
+        user.rooms = user.rooms.filter((r) => r !== roomId.id);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    if (parsedData.type === "chat") {
+      try {
+        const user = users.find((u) => u.ws === ws && u.userId === userId);
+        if (!user) {
+          return;
+        }
+        const message = parsedData.message;
+        if (!message) {
+          return;
+        }
+
+        const roomId = decode(parsedData.roomId);
+        let recipients = users.filter((u) => u.rooms.includes(roomId.id));
+
+        await prisma.chat.create({
+          data: {
+            message,
+            user: {
+              connect: {
+                id: userId,
+              },
+            },
+            room: {
+              connect: {
+                id: roomId.id,
+              },
+            },
+          },
+        });
+
+        recipients.forEach((u) => {
+          try {
+            u.ws.send(
+              JSON.stringify({
+                type: "message",
+                message: message,
+                roomId: roomId.id,
+              }),
+            );
+          } catch (sendError) {
+            console.error(
+              `Error sending message to user ${u.userId}:`,
+              sendError,
+            );
+          }
+        });
+      } catch (error) {
+        console.error("An error occurred while processing chat:", error);
+      }
+    } else {
+      console.log("Unknown message type:", parsedData.type);
+    }
   });
 });
